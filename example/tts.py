36157a4b353be6283cb35dcc808ef31cd31171c1
Akagi201 <akagi201@gmail.com>
2015-09-01 22:24:33 +0800

    add ttx example

---
+++ example/tts.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+import PyBaiduYuyin as pby
+tts = pby.TTS(app_key='8EEdQ1cwlXKwc9sg5owY2qma', secret_key='ac9db65885f25bbf4d327f36c9a26a65')
+tts.say("你好")
